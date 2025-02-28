804195e7f98cd95f9bed890242cb3e8999400e5b
BradyUT <bradycb2004@gmail.com>
2025-02-28 11:31:57 -0600

    Made dice stuff?

---
+++ dice/risky/risky_dice.gd
@@ -0,0 +1,6 @@
+extends Dice
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	type = Dice.Type.RISKY
---
+++ dice/standard/standard_dice.gd
@@ -0,0 +1,24 @@
+extends Dice
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func roll_die(faces) -> void:
+	var roll = randi_range(1, faces)
+	result = roll
+	emit_signal("rolled")
+	$AnimatedSprite2D.animation = "roll"
+	$AnimatedSprite2D.play()
+	await $AnimatedSprite2D.animation_finished
+	$AnimatedSprite2D.stop()
+	$Label.text = "You rolled a " + str(roll)
+	$AnimatedSprite2D.animation = "faces"
+	$AnimatedSprite2D.frame = roll - 1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
