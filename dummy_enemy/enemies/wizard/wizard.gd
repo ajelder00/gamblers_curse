6b742e2e6a24e8b2c065746b22737d190f3ec86a
BradyUT <bradycb2004@gmail.com>
2025-03-28 01:04:05 -0500

    Few tweaks

--- dummy_enemy/enemies/wizard/wizard.gd
+++ dummy_enemy/enemies/wizard/wizard.gd
@@ -1,11 +1,16 @@
-extends Node2D
+extends DummyEnemy
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	base_accuracy = 1.0
+	immunities = [Dice.Type.POISON]
+	dice_bag = [$Dice, $Dice2]
+	type = DummyEnemy.Type.WIZARD
+	dice = $Dice
+	dice_button = $Dice/Button
+	turns = 3
+	health = 60
+	tier = 3
+	coins = int(randi_range(0,10)*randf_range(tier,tier + 1))
+	super._ready()
